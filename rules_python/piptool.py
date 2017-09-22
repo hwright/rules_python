ac673a50d0d84b542918b6902b7be2149fab9ed7
Matt Moore <mattmoor@google.com>
2017-09-22 23:40:27 +0000

    This adds support for "extras".
    "Extras" are additional dependencies of a given library, which are consumed by passing the "extra" name in brackets after the distribution name, for example:
    ```
    mock[docs]==1.0.1
    ```
    
    We see this in the dependencies of several Google Cloud libraries, which depend on: `googleapis_common_protos[grpc]`
    
    I've added a simple test that the dependency structure we synthesize for this kind of thing is correct via an "extras" test that has a `requirements.txt` of:
    ```
    google-cloud-language==0.27.0
    ```
    
    Fixes: https://github.com/bazelbuild/rules_python/issues/12

--- rules_python/piptool.py
+++ rules_python/piptool.py
@@ -17,63 +17,29 @@ import argparse
 import json
 import os
 import pkgutil
+import pkg_resources
 import re
 import sys
 import tempfile
 import zipfile
 
-# PIP erroneously emits an error when bundled as a PAR file.  We
-# disable the version check to silence it.
-try:
-  # Make sure we're using a suitable version of pip as a library.
-  # Fallback on using it as a CLI.
-  from pip._vendor import requests
+# Make sure we're using a suitable version of pip as a library.
+# Fallback on using it as a CLI.
+from pip._vendor import requests
 
-  from pip import main as _pip_main
-  def pip_main(argv):
-    # Extract the certificates from the PAR following the example of get-pip.py
-    # https://github.com/pypa/get-pip/blob/430ba37776ae2ad89/template.py#L164-L168
-    cert_path = os.path.join(tempfile.mkdtemp(), "cacert.pem")
-    with open(cert_path, "wb") as cert:
-      cert.write(pkgutil.get_data("pip._vendor.requests", "cacert.pem"))
-    argv = ["--disable-pip-version-check", "--cert", cert_path] + argv
-    return _pip_main(argv)
+from pip import main as _pip_main
+def pip_main(argv):
+  # Extract the certificates from the PAR following the example of get-pip.py
+  # https://github.com/pypa/get-pip/blob/430ba37776ae2ad89/template.py#L164-L168
+  cert_path = os.path.join(tempfile.mkdtemp(), "cacert.pem")
+  with open(cert_path, "wb") as cert:
+    cert.write(pkgutil.get_data("pip._vendor.requests", "cacert.pem"))
+  # PIP erroneously emits an error when bundled as a PAR file.  We
+  # disable the version check to silence it.
+  argv = ["--disable-pip-version-check", "--cert", cert_path] + argv
+  return _pip_main(argv)
 
-except:
-  import subprocess
-
-  def pip_main(argv):
-    return subprocess.call(['pip'] + argv)
-
-# TODO(mattmoor): We can't easily depend on other libraries when
-# being invoked as a raw .py file.  Once bundled, we should be able
-# to remove this fallback on a stub implementation of Wheel.
-try:
-  from rules_python.whl import Wheel
-except:
-  class Wheel(object):
-
-    def __init__(self, path):
-      self._path = path
-
-    def basename(self):
-      return os.path.basename(self._path)
-
-    def distribution(self):
-      # See https://www.python.org/dev/peps/pep-0427/#file-name-convention
-      parts = self.basename().split('-')
-      return parts[0]
-
-    def version(self):
-      # See https://www.python.org/dev/peps/pep-0427/#file-name-convention
-      parts = self.basename().split('-')
-      return parts[1]
-
-    def repository_name(self):
-      # Returns the canonical name of the Bazel repository for this package.
-      canonical = 'pypi__{}_{}'.format(self.distribution(), self.version())
-      # Escape any illegal characters with underscore.
-      return re.sub('[-.]', '_', canonical)
+from rules_python.whl import Wheel
 
 parser = argparse.ArgumentParser(
     description='Import Python dependencies into Bazel.')
@@ -90,6 +56,59 @@ parser.add_argument('--output', action='store',
 parser.add_argument('--directory', action='store',
                     help=('The directory into which to put .whl files.'))
 
+def determine_possible_extras(whls):
+  """Determines the list of possible "extras" for each .whl
+
+  The possibility of an extra is determined by looking at its
+  additional requirements, and determinine whether they are
+  satisfied by the complete list of available wheels.
+
+  Args:
+    whls: a list of Wheel objects
+
+  Returns:
+    a dict that is keyed by the Wheel objects in whls, and whose
+    values are lists of possible extras.
+  """
+  whl_map = {
+    whl.distribution(): whl
+    for whl in whls
+  }
+
+  # TODO(mattmoor): Consider memoizing if this recursion ever becomes
+  # expensive enough to warrant it.
+  def is_possible(distro, extra):
+    distro = distro.replace("-", "_")
+    # If we don't have the .whl at all, then this isn't possible.
+    if distro not in whl_map:
+      return False
+    whl = whl_map[distro]
+    # If we have the .whl, and we don't need anything extra then
+    # we can satisfy this dependency.
+    if not extra:
+      return True
+    # If we do need something extra, then check the extra's
+    # dependencies to make sure they are fully satisfied.
+    for extra_dep in whl.dependencies(extra=extra):
+      req = pkg_resources.Requirement.parse(extra_dep)
+      # Check that the dep and any extras are all possible.
+      if not is_possible(req.project_name, None):
+        return False
+      for e in req.extras:
+        if not is_possible(req.project_name, e):
+          return False
+    # If all of the dependencies of the extra are satisfiable then
+    # it is possible to construct this dependency.
+    return True
+
+  return {
+    whl: [
+      extra
+      for extra in whl.extras()
+      if is_possible(whl.distribution(), extra)
+    ]
+    for whl in whls
+  }
 
 def main():
   args = parser.parse_args()
@@ -106,6 +125,9 @@ def main():
         if fname.endswith('.whl'):
           yield os.path.join(root, fname)
 
+  whls = [Wheel(path) for path in list_whls()]
+  possible_extras = determine_possible_extras(whls)
+
   def whl_library(wheel):
     # Indentation here matters.  whl_library must be within the scope
     # of the function below.  We also avoid reimporting an existing WHL.
@@ -115,10 +137,25 @@ def main():
         name = "{repo_name}",
         whl = "@{name}//:{path}",
         requirements = "@{name}//:requirements.bzl",
+        extras = [{extras}]
     )""".format(name=args.name, repo_name=wheel.repository_name(),
-              path=wheel.basename())
+                path=wheel.basename(),
+                extras=','.join([
+                  '"%s"' % extra
+                  for extra in possible_extras.get(wheel, [])
+                ]))
 
-  whls = [Wheel(path) for path in list_whls()]
+  whl_targets = ','.join([
+    ','.join([
+      '"%s": "@%s//:pkg"' % (whl.distribution().lower(), whl.repository_name())
+    ] + [
+      # For every extra that is possible from this requirements.txt
+      '"%s[%s]": "@%s//:%s"' % (whl.distribution().lower(), extra.lower(),
+                                whl.repository_name(), extra)
+      for extra in possible_extras.get(whl, [])
+    ])
+    for whl in whls
+  ])
 
   with open(args.output, 'w') as f:
     f.write("""\
@@ -142,10 +179,7 @@ def requirement(name):
   return _requirements[name]
 """.format(input=args.input,
            whl_libraries='\n'.join(map(whl_library, whls)),
-           mappings=','.join([
-             '"%s": "@%s//:pkg"' % (wheel.distribution().lower(), wheel.repository_name())
-             for wheel in whls
-           ])))
+           mappings=whl_targets))
 
 if __name__ == '__main__':
   main()
